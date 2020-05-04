4f5a83fe4f3fefbb06b7fd58d24e54771ede6768
Idan Shemy <ishemy@ucdavis.edu>
2020-05-04 16:48:02 -0700

    teletype bnabyyyyyy

--- ssqsim.py
+++ ssqsim.py
@@ -7,8 +7,13 @@ ARRIVAL = 0
 DEPARTURE = 1
 
 class Event:
+	# these are variables
 	event-time
 	event-type
 
+	# this is a constructor
+	# what the fuck is uppppppppp
+	# nothing much papi I miss you
+	#
 	def __init__(self):
 		printf("hi")
