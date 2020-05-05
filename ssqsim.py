3552e7b77ccc595f48435f58dc03aab3c6b3a3e6
Idan Shemy <ishemy@ucdavis.edu>
2020-05-04 17:30:54 -0700

    added linkedlist file, started some stuff in regular file

--- ssqsim.py
+++ ssqsim.py
@@ -2,18 +2,53 @@
 from collections import deque
 from enum import Enum
 
-MAXBUFFER
 ARRIVAL = 0
 DEPARTURE = 1
 
 class Event:
 	# these are variables
-	event-time
-	event-type
+	event_time # For an arrival event it is the time the packet arrives at the transmitter and for a departure event it is the time when the server is finished transmitting the packet.
+	event_type # arrival or departure
+	next_event # ptr to next event
+	prev_event # ptr to prev event
 
 	# this is a constructor
-	# what the fuck is uppppppppp
-	# nothing much papi I miss you
-	#
 	def __init__(self):
 		printf("hi")
+
+
+
+class SingleServerQueue:
+	"""d main class"""
+
+	MAXBUFFER
+	length
+	time
+	service_rate
+	arrival_rate
+
+	gel = deque()
+	buffer = deque()
+
+	# default constructor
+	def __init__(self, service_rate, arrival_rate):
+		super(SingleServerQueue, self).__init__()
+		self.length 	  = 0
+		self.time   	  = 0
+		self.service_rate = service_rate
+		self.arrival_rate = arrival_rate
+
+
+	def clock(arg):
+		for i in 100000:
+			firstEvent = gel.popLeft()
+			if (firstEvent.event_time == ARRIVAL):
+				#process arival_Event
+			else:
+				#process service_completion
+				print ("hey")
+
+
+	def processArival(arg):
+		curr_time = event_time
+		if ()
