b5fe0a4ca6a1d7f1a47c3d52436b340edb492089
Idan Shemy <ishemy@ucdavis.edu>
2020-05-06 04:49:40 -0700

    fini

--- ssqsim.py
+++ ssqsim.py
@@ -2,7 +2,6 @@
 from collections import deque
 from random import random
 from math import log
-#import linkedlist
 from linkedlist import *
 
 ARRIVAL = 0
@@ -23,18 +22,15 @@ class SingleServerQueue:
 	# default constructor
 	def __init__(self, lam, MAXBUFFER):
 		self.MAXBUFFER         = MAXBUFFER
-		self.sum_queue_length  = 0
-		self.mean_queue_length = 0
-		self.server_busy_time  = 0
-		self.mean_server_util  = 0
 		self.mu 		       = 1
 		self.lam  			   = lam
 		self.time   	       = exponential_random(lam)
 		self.packets_dropped   = 0
-		self.gel = LinkedList()
-		self.gel.head = Node(Event(self.time, exponential_random(self.mu), ARRIVAL))
-		self.buffer = deque(maxlen = MAXBUFFER)
-		self.length = 0
+		self.gel 			   = LinkedList()
+		self.gel.head          = Node(Event(self.time, exponential_random(self.mu), ARRIVAL))
+		self.buffer 		   = deque(maxlen = MAXBUFFER)
+		self.length 		   = 0
+		self.server_busy_time  = self.gel.head.data.service_time
 
 	def add_to_gel(self, event):
 		if (self.gel.is_empty):
@@ -51,6 +47,7 @@ class SingleServerQueue:
 		next_arrival_time = self.time + exponential_random(self.lam)
 		next_service_time = exponential_random(self.mu)
 		next_arrival_event = Event(next_arrival_time, next_service_time, ARRIVAL)
+		self.server_busy_time += next_service_time
 		self.add_to_gel(next_arrival_event)
 
 		if (len(self.buffer) == 0):
@@ -60,48 +57,48 @@ class SingleServerQueue:
 		else:
 			if (self.length - 1 < MAXBUFFER):
 				print("buffer is full")
-				packets_dropped += 1 # the queue is full and therefore the packet is dropped
-			else:
-				server_busy_time += event.service_time
-				print("buffer is not full")
 				self.length += 1
 				self.buffer.append(event)
-
-		self.sum_queue_length += self.length
-		self.mean_queue_length = float(sum_queue_length) / float(self.time)
-		self.server_busy_time += event.service_time
-		self.mean_server_util = float(server_busy_time) / float(self.time)
+			else:
+				print("buffer full")
+				packets_dropped += 1 # the queue is full and therefore the packet is dropped
 
 	def process_departure(self, event):
 		self.time = event.event_time
-		self.mean_queue_length = float(self.sum_queue_length) / float(self.time)
-		self.mean_server_util = float(self.server_busy_time) / float(self.time)
 		self.length -= 1
 
-		if (len(self.buffer) > 0):
+		if (self.length > 0):
 
 			departure_event = self.buffer.popLeft()
-			departure_event.event_time = self.time +  departure_event.service_time
+			departure_event.event_time = self.time + departure_event.service_time
+			departure_event.service_time = 0
 			departure_event.event_type = DEPARTURE
 
 			self.add_to_gel(departure_event)
 
-			self.sum_queue_length += len(self.buffer)
-			self.mean_queue_length = float(self.sum_queue_length) / float(self.time)
-
 # main functions
-server = SingleServerQueue(0.0001, 1)
+server            = SingleServerQueue(0.02, 20)
+sum_time          = 0
+mean_queue_length = 0
+mean_server_util  = 0
 
 for i in range(10):
 	nextEvent = server.gel.head.data
 	server.gel.remove_node(server.gel.head.data)
+
+	if (i != 0):
+		mean_queue_length += server.length * (server.time - sum_time)
 	# same as transmission time for an event or size for a packet
 	if (nextEvent.event_type == ARRIVAL):
 		server.process_arrival(nextEvent)
 	else:
 		server.process_departure(nextEvent)
 
-print(server.server_busy_time)
-print(server.mean_queue_length)
-print(server.mean_server_util)
+	sum_time = nextEvent.event_time
+
+mean_server_util = (float)(server.server_busy_time)/(float)(server.time)
+
+#print(server.server_busy_time)
+print(mean_queue_length)
+print(mean_server_util * 100.0)
 print(server.packets_dropped)
